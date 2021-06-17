680c4aa9544da546a9232c7125f0df2ab7c9d560
lokeshvarmapasupuleti <75163225+lokeshvarmapasupuleti@users.noreply.github.com>
2021-06-17 16:56:25 +0530

    Add files via upload

---
+++ PRO-C43-Template-main/js/Code.js
@@ -0,0 +1,23 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displayin fill("white")
+    textSize(15)
+    text(" C U T N I F O N", 700,50)
+    fill("lightblue")
+    text("Hint: something that perform a particular task !!", 100,250)
+    textSize(15)
+    text(" D T A B S E A A", 100,50)
+    fill("lightblue")
+    text("Hint: something that perform a particular task !!", 100,70)
+
+
+}
---
+++ PRO-C43-Template-main/js/System.js
@@ -0,0 +1,17 @@
+class System{
+
+    constructor(){}
+
+    authenticate(actualCode,enteredCode){
+        textSize(50);
+        fill("black");
+        text(code,300,300)
+        if(actualCode === enteredCode)
+        //add code to return true if correct answer entered.
+     return true 
+     else 
+     return false
+        
+    }
+
+}
